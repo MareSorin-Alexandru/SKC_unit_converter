222aef97939843dae65a29e883b777923c379fd9
sm18240 <sm18240@essex.ac.uk>
2022-06-26 20:14:19 +0100

    removed unneeded casts

--- fuel_consumption.py
+++ fuel_consumption.py
@@ -4,39 +4,39 @@ from auxilliary_functions import *
 
 # us_mpg to l/00Km
 def fc_us_mpgtol100KM(fc_us_mpg):
-    return float(format(235.237/fc_us_mpg, ".2f"))
+    return format(235.237/fc_us_mpg, ".2f")
 
 # us_mpg to uk_mpg
 def fc_us_mpgtouk_mpg(fc_us_mpg):
-    return float(format(fc_us_mpg*1.201,".2f"))
+    return format(fc_us_mpg*1.201,".2f")
 
 # us_mpg to us_mpg
 def fc_us_mpgtous_mpg(fc_us_mpg):
-    return float(format(fc_us_mpg,".2f"))
+    return format(fc_us_mpg,".2f")
 
 # uk_mpg to us_mpg
 def fc_uk_mpgtous_mpg(fc_uk_mpg):
-    return float(format(fc_uk_mpg*0.832,".2f"))
+    return format(fc_uk_mpg*0.832,".2f")
 
 # uk_mpg to l/100km
 def fc_uk_mpgtol100km(fc_uk_mpg):
-    return float(format(282.533/fc_uk_mpg,".2f"))
+    return format(282.533/fc_uk_mpg,".2f")
 
 # uk_mpg to uk_mpg
 def fc_uk_mpgtouk_mpg(fc_uk_mpg):
-    return float(format(fc_uk_mpg,".2f"))
+    return format(fc_uk_mpg,".2f")
 
 # l/100Km to us_mpg
 def fc_l100KMtous_mpg(fc_l100km):
-    return float(format(235.237/fc_l100km, ".2f"))
+    return format(235.237/fc_l100km, ".2f")
 
 # l/100km to uk_mpg
 def fc_l100kmtouk_mpg(fc_l100km):
-    return float(format(282.533/fc_l100km,".2f"))
+    return format(282.533/fc_l100km,".2f")
 
 # l/100km to l/100km
 def fc_l100KMtol100KM(fc_l100km):
-    return float(format(fc_l100km,".2f"))
+    return format(fc_l100km,".2f")
 
 fuel_economy_func_map = {
     "fc_us_mpgl100km" : fc_us_mpgtol100KM,
@@ -58,30 +58,30 @@ def fc_tests():
     print("from to input_v true_v test_v")
 
     print("l100km us_mpg 16 14.7",fc_l100KMtous_mpg(16))
-    assert_almost_equal_float(fuel_economy_func_map.get("fc_l100kmus_mpg")(16),14.7)
+    assert_almost_equal_float( float(fuel_economy_func_map.get("fc_l100kmus_mpg")(16)),14.7)
 
     print("l100km uk_mpg 16 17.66",fc_l100kmtouk_mpg(16))
-    assert_almost_equal_float(fuel_economy_func_map.get("fc_l100kmuk_mpg")(16),17.66)
+    assert_almost_equal_float( float(fuel_economy_func_map.get("fc_l100kmuk_mpg")(16)),17.66)
 
     print("l100km l100km 5 5",fc_l100KMtol100KM(5))
-    assert_almost_equal_float(fuel_economy_func_map.get("fc_l100kml100km")(5),5)
+    assert_almost_equal_float( float(fuel_economy_func_map.get("fc_l100kml100km")(5)),5)
 
 
     print("us_mpg l100km 14 16.8",fc_us_mpgtol100KM(14))
-    assert_almost_equal_float(fuel_economy_func_map.get("fc_us_mpgl100km")(14), 16.8)
+    assert_almost_equal_float( float(fuel_economy_func_map.get("fc_us_mpgl100km")(14)), 16.8)
 
     print("us_mpg uk_mpg 5 6",fc_us_mpgtouk_mpg(5))
-    assert_almost_equal_float(fuel_economy_func_map.get("fc_us_mpguk_mpg")(5),6)
+    assert_almost_equal_float( float(fuel_economy_func_map.get("fc_us_mpguk_mpg")(5)),6)
 
     print("us_mpg us_mpg 5 5",fc_us_mpgtous_mpg(5))
-    assert_almost_equal_float(fuel_economy_func_map.get("fc_us_mpgus_mpg")(5),5)
+    assert_almost_equal_float( float(fuel_economy_func_map.get("fc_us_mpgus_mpg")(5)),5)
 
 
     print("uk_mpg l100km 14 20.18",fc_uk_mpgtol100km(14))
-    assert_almost_equal_float(fuel_economy_func_map.get("fc_uk_mpgl100km")(14), 20.18)
+    assert_almost_equal_float( float(fuel_economy_func_map.get("fc_uk_mpgl100km")(14)), 20.18)
 
     print("uk_mpg us_mpg 14 11.65",fc_uk_mpgtous_mpg(14))
-    assert_almost_equal_float(fuel_economy_func_map.get("fc_uk_mpgus_mpg")(14),11.65)
+    assert_almost_equal_float( float(fuel_economy_func_map.get("fc_uk_mpgus_mpg")(14)),11.65)
 
     print("uk_mpg uk_mpg 22 22",fc_uk_mpgtouk_mpg(22))
-    assert_almost_equal_float(fuel_economy_func_map.get("fc_uk_mpguk_mpg")(22),22)
+    assert_almost_equal_float( float(fuel_economy_func_map.get("fc_uk_mpguk_mpg")(22)),22)
--- temperature.py
+++ temperature.py
@@ -10,39 +10,39 @@ Its not addressed here
 
 # F to C
 def t_FtoC(f_temp):
-    return float(format((f_temp-32)*(5/9),".2f"))
+    return format((f_temp-32)*(5/9),".2f")
 
 # F to K
 def t_FtoK(f_temp):
-    return float(format((f_temp-32)*(5/9) + 273.15 ,  ".2f"))
+    return format((f_temp-32)*(5/9) + 273.15 ,  ".2f")
 
 # F to F
 def t_FtoF(f_temp):
-    return float(format(f_temp,".2f"))
+    return format(f_temp,".2f")
 
 # C to F
 def t_CtoF(c_temp):
-    return float(format(((9/5)*c_temp)+32 ,  ".2f"))
+    return format(((9/5)*c_temp)+32 ,  ".2f")
 
 # C to K
 def t_CtoK(c_temp):
-    return float(format(c_temp + 273.15,".2f"))
+    return format(c_temp + 273.15,".2f")
 
 # C to C
 def t_CtoC(c_temp):
-    return float(format(c_temp,".2f"))
+    return format(c_temp,".2f")
 
 # K to C
 def t_KtoC(k_temp):
-    return float(format(k_temp - 273.15, ".2f"))
+    return format(k_temp - 273.15, ".2f")
 
 # K to F
 def t_KtoF(k_temp):
-    return float(format(((9/5)*(k_temp-273.15))+32 ,  ".2f"))
+    return format(((9/5)*(k_temp-273.15))+32 ,  ".2f")
 
 # K to K
 def t_KtoK(k_temp):
-    return float(format(k_temp,".2f"))
+    return format(k_temp,".2f")
 
 temp_func_map = {
     "t_fc":t_FtoC,
@@ -64,30 +64,30 @@ def temp_test():
     print("from to input_v true_v test_v")
 
     print("f c -40 -40",t_FtoC(-40))
-    assert_almost_equal_float( temp_func_map.get("t_fc")(-40), -40 )
+    assert_almost_equal_float( float(temp_func_map.get("t_fc")(-40)), -40 )
 
     print("f k -40 233.15",t_FtoK(-40))
-    assert_almost_equal_float( temp_func_map.get("t_fk")(-40), 233.15 )
+    assert_almost_equal_float( float(temp_func_map.get("t_fk")(-40)), 233.15 )
     
     print("f f 0 0",t_FtoF(0))
-    assert_almost_equal_float( temp_func_map.get("t_ff")(0), 0 )
+    assert_almost_equal_float( float(temp_func_map.get("t_ff")(0)), 0 )
 
 
     print("k c 0 -273.15",t_KtoC(0))
-    assert_almost_equal_float( temp_func_map.get("t_kc")(0), -273.15 )
+    assert_almost_equal_float( float(temp_func_map.get("t_kc")(0)), -273.15 )
 
     print("k f 0 -459.67",t_KtoF(0))
-    assert_almost_equal_float( temp_func_map.get("t_kf")(0), -459.67 )
+    assert_almost_equal_float( float(temp_func_map.get("t_kf")(0)), -459.67 )
 
     print("k k 0 0", t_KtoK(0))
-    assert_almost_equal_float( temp_func_map.get("t_kk")(0), 0 )
+    assert_almost_equal_float( float(temp_func_map.get("t_kk")(0)), 0 )
     
 
     print("c f -40 -40", t_CtoF(-40))
-    assert_almost_equal_float( temp_func_map.get("t_cf")(-40), -40 )
+    assert_almost_equal_float( float(temp_func_map.get("t_cf")(-40)), -40 )
 
     print("c k 0 273.15",t_CtoK(0))
-    assert_almost_equal_float( temp_func_map.get("t_ck")(0), 273.15 )
+    assert_almost_equal_float( float(temp_func_map.get("t_ck")(0)), 273.15 )
 
     print("c c 0 0", t_CtoC(0))
-    assert_almost_equal_float( temp_func_map.get("t_cc")(0), 0 )
+    assert_almost_equal_float( float(temp_func_map.get("t_cc")(0)), 0 )
